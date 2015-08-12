bb979e49de660506326aef05b437e3d571c8367d
ProximaDas <prox.me@gmail.com>
2015-08-11 23:23:10 -0700

    HW06

--- HW06_ex09_04.py
+++ HW06_ex09_04.py
@@ -10,12 +10,12 @@
 # alfalfa?"
 #   - write function to assist you
 #   - type favorite sentence(s) here:
-#       1:
-#       2:
-#       3:
+#       1: reproachful coalfishes
+#       2: coalfishes reproachfully
+#       3: coalfishes reproachfulnesses
 ##############################################################################
 # Imports
-
+import itertools
 # Body
 def uses_only(word,letters):
 	flag = False
@@ -27,10 +27,38 @@ def uses_only(word,letters):
 			break
 	return flag		
 			
+def make_sentence():
+	letters = "acefhlo"
+	flag = False
+	word_list = []
+	with open("words.txt","r") as handler:
+		words = handler.read().split()
+		for word in words:
+			for letter in letters:
+				if letter in word:
+					flag = True
+				else:
+					flag = False
+					break
+			if flag == True:
+				word_list.append(word)
+	print ' '.join(word_list)
 
+# def combination():
+# 	list_ = list('acefhlo')
+# 	new = itertools.chain.from_iterable(itertools.combinations(list_,i) for i in range(len(list_)+1))
+# 	# print list(new)
+# 	while True:
+# 		try:
+# 			print ''.join(new.next())
+# 		except:
+# 			break
+			
 ##############################################################################
 def main():
     print uses_only("banana","aBn")
+    make_sentence()
+    combination()
 
 if __name__ == '__main__':
     main()
